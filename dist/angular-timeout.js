5406aaf110a3bf93feca752e2360d619ec5ce304
Ruben Vermeersch <ruben@rocketeer.be>
2015-02-04 11:23:52 +0100

    Initial commit.

---
+++ dist/angular-timeout.js
@@ -0,0 +1,31 @@
+angular.module("rt.timeout", []).factory("scopedTimeout", ["$timeout", "$interval", function ($timeout, $interval) {
+    return function ($scope) {
+        var timeouts = [];
+        var intervals = [];
+
+        function destroyAll(service, promises) {
+            for (var i = 0; i < promises.length; i++) {
+                service.cancel(promises[i]);
+            }
+        }
+
+        $scope.$on("$destroy", function () {
+            destroyAll($timeout, timeouts);
+            destroyAll($interval, intervals);
+        });
+
+        return {
+            timeout: function (fn, ts) {
+                var promise = $timeout(fn, ts);
+                timeouts.push(promise);
+                return promise;
+            },
+
+            interval: function (fn, ts) {
+                var promise = $interval(fn, ts);
+                intervals.push(promise);
+                return promise;
+            },
+        };
+    };
+}]);
